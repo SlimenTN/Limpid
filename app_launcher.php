64cea9d77a6aae58d8082448eed59a5b7c845c5f
Brux <Brux@192.168.1.100>
2016-12-31 18:39:34 +0100

    Improve exceptions handler for dev and prod mode + debug barre

--- app_launcher.php
+++ app_launcher.php
@@ -12,18 +12,5 @@ require_once 'vendor/autoload.php';
  */
 use framework\core\Controller\CrossRoadsRooter;
 
-// point of start
-$timestart=microtime(true);
-
 $crossRoadesRouter = new CrossRoadsRooter();
 $crossRoadesRouter->parseRequest();
-
-//End of PHP execution
-$timeend=microtime(true);
-$time=$timeend-$timestart;
-
-//Display execution time
-$page_load_time = number_format($time, 3);
-echo '<div style="width: 100%; background: gray; color: #fff;position: fixed;
-bottom: 0;
-padding: 5px;">Script execute en '.$page_load_time.' sec</div>';
